efef988fe3287dc29eeae732c2a9cf3ac14de1bc
Shabbir33 <shabbirpoonawala03@gmail.com>
2024-10-11 23:16:34 +0530

    Convex Mutations for Boards implemented. Board List Component and New Board Button done.

--- app/(dashboard)/[[...create-organization]]/_components/BoardList.tsx
+++ app/(dashboard)/[[...create-organization]]/_components/BoardList.tsx
@@ -1,6 +1,13 @@
 "use client";
 
+import { useQuery } from "convex/react";
+
+import { api } from "@/convex/_generated/api";
+
+import { EmptyBoards } from "./EmptyBoards";
 import { EmptyState } from "./EmptyState";
+import { BoardCard } from "./BoardCard";
+import { NewBoardButton } from "./NewBoardButton";
 
 interface BoardListProps {
   orgId: string;
@@ -11,7 +18,24 @@ interface BoardListProps {
 }
 
 const BoardList = ({ orgId, query }: BoardListProps) => {
-  const data = []; // TODO: Change to API call
+  const data = useQuery(api.boards.get, { orgId });
+
+  if (data === undefined) {
+    return (
+      <div>
+        <h2 className="text-3xl">
+          {query.favourites ? "Favourites boards" : "Team boards"}
+        </h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+          <NewBoardButton orgId={orgId} disabled />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+        </div>
+      </div>
+    );
+  }
 
   if (!data?.length && query.search) {
     return (
@@ -36,17 +60,31 @@ const BoardList = ({ orgId, query }: BoardListProps) => {
   }
 
   if (!data?.length) {
-    return (
-      <EmptyState
-        src="/empty-boards.svg"
-        heading="Create your first board!"
-        desc="Start by creating a board for your organization"
-        size={{ height: 110, width: 110 }}
-        button="Create board"
-      />
-    );
+    return <EmptyBoards />;
   }
-  return <div>BoardList</div>;
+  return (
+    <div>
+      <h2 className="text-3xl">
+        {query.favourites ? "Favourites boards" : "Team boards"}
+      </h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+        <NewBoardButton orgId={orgId} />
+        {data?.map((board) => (
+          <BoardCard
+            key={board._id}
+            id={board._id}
+            title={board.title}
+            imageUrl={board.imageUrl}
+            authorId={board.authorId}
+            authorName={board.authorName}
+            createdAt={board._creationTime}
+            orgId={board.orgId}
+            isFavourite={false}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default BoardList;
---
+++ hooks/useApiMutation.ts
@@ -0,0 +1,26 @@
+// Hook for integrating Mutation and Loading state in a single hook call.
+
+import { useState } from "react";
+import { useMutation } from "convex/react";
+
+export const useApiMutation = (mutationFunction: any) => {
+  const [pending, setPending] = useState(false);
+  const apiMutation = useMutation(mutationFunction);
+
+  const mutate = (payload: any) => {
+    setPending(true);
+    return apiMutation(payload)
+      .finally(() => setPending(false))
+      .then((result) => {
+        return result;
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
+
+  return {
+    mutate,
+    pending,
+  };
+};
